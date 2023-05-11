ac505d36228f6636db38647c8dd4a08fbe622884
IvanA13 <iaamayab@ufpso.edu.co>
2023-05-10 21:17:45 -0500

    bienvenido a js

---
+++ js/clase.js
@@ -0,0 +1,39 @@
+// VARIABLES
+const nameuser="IvanAmaya";
+var x = "valor";
+let y = "valor";
+
+let a = 20;
+let b= 5.4;
+let string ="Mi nombre es Ivan Amaya";
+let number = "023456789";
+let boolean_Uno=false;
+let boolean_Two=true;
+let array=[1,2,3,4,5,6];
+let array_string=["lunes,martes, miercoles,jueves,viernes"];
+
+//Operadores aritmeticos: + - / * %
+let suma=1+5+a;
+let resta=1-5-a;
+let multi=1*5*a;
+let divi=1/5/a;
+let module=10%2;
+let elev= 5^2;
+console.log("- "+suma+"\n- "+resta+"\n-"+multi+"\n-"+divi+"\n-"+module+"\n-"+elev);
+
+//Operadores de Comparacion =/ == / === / <> / >= / <= /
+
+
+
+if(10>5){
+    for(let i=0;i>array_string.length;i++){
+        console.log(array_string[i]);
+    }
+}
+
+let j=5;
+let k=10;
+while(k>=j){
+    console.log("ENTRA");
+    j++;
+}
